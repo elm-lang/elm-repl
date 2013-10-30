620a0d46505e300faecfa9d2bc1e5826eaf6183b
Evan Czaplicki <info@elm-lang.org>
2013-10-30 19:44:38 +0100

    Start using haskeline to manage history and arrow key inputs

---
+++ Evaluator.hs
@@ -0,0 +1,85 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Evaluator where
+
+import qualified Data.ByteString.Char8 as BSC
+import qualified Data.ByteString as BS
+import qualified Data.Map as Map
+import qualified Language.Elm as Elm
+import qualified Environment as Env
+
+import System.IO.Error  (isDoesNotExistError)
+import System.Directory (removeFile)
+import System.Exit      (ExitCode(..))
+import System.FilePath  ((</>), replaceExtension)
+import System.Process
+import Control.Exception
+
+
+runRepl :: Env.Repl -> IO Bool
+runRepl env =
+  do writeFile tempElm (Env.toElm env)
+     result <- onSuccess compile $ \types -> do
+       reformatJS tempJS
+       onSuccess run $ \value' ->
+           let value = BSC.init value'
+               tipe = scrapeOutputType types
+               isTooLong = or [ BSC.isInfixOf "\n" value
+                              , BSC.isInfixOf "\n" tipe
+                              , BSC.length value + BSC.length tipe > 80 ]    
+               message = BS.concat [ if isTooLong then value' else value, tipe ]
+           in  if BSC.null value' then return () else BSC.putStrLn message
+     removeIfExists tempElm
+     return result
+  where
+    tempElm = "repl-temp-000.elm"
+    tempJS  = "build" </> replaceExtension tempElm "js"
+    
+    run = (proc "node" [tempJS]) { std_out = CreatePipe }
+    compile = (proc "elm" args) { std_out = CreatePipe }
+        where args = [ "--make", "--only-js", "--print-types", tempElm ]
+
+    onSuccess action success =
+      let failure message = BSC.putStrLn message >> return False in
+      do (_, stdout, _, handle) <- createProcess action
+         exitCode <- waitForProcess handle
+         case (exitCode, stdout) of
+           (ExitFailure 127, _)      -> failure "Error: elm binary not found in your path."
+           (_, Nothing)              -> failure "Unknown error!"
+           (ExitFailure _, Just out) -> failure =<< BS.hGetContents out
+           (ExitSuccess  , Just out) ->
+               do success =<< BS.hGetContents out
+                  return True
+
+reformatJS :: String -> IO ()
+reformatJS tempJS =
+  do rts <- BS.readFile =<< Elm.runtime
+     src <- BS.readFile tempJS
+     BS.length src `seq` BS.writeFile tempJS (BS.concat [rts,src,out])
+  where
+    out = BS.concat
+          [ "var context = { inputs:[] };\n"
+          , "var repl = Elm.Repl.make(context);\n"
+          , "if (repl.", Env.output, ")\n"
+          , "  console.log(context.Native.Show.values.show(repl.", Env.output, "));" ]
+
+scrapeOutputType types
+    | name == Env.output = tipe
+    | BS.null rest       = ""
+    | otherwise          = scrapeOutputType rest
+    where
+      (next,rest) = freshLine types
+      (name,tipe) = BSC.splitAt (BSC.length Env.output) next
+
+      freshLine str
+          | BSC.take 2 rest == "\n " = (BS.append line line', rest')
+          | BS.null rest = (line,"")
+          | otherwise    = (line, BS.tail rest)
+          where
+            (line,rest) = BSC.break (=='\n') str
+            (line',rest') = freshLine rest
+
+removeIfExists :: FilePath -> IO ()
+removeIfExists fileName = removeFile fileName `catch` handleExists
+  where handleExists e
+          | isDoesNotExistError e = return ()
+          | otherwise = throwIO e
--- Repl.hs
+++ Repl.hs
@@ -1,111 +1,31 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import qualified Data.ByteString.Char8 as BSC
-import qualified Data.ByteString as BS
-import qualified Data.Map as Map
-import qualified Language.Elm as Elm
-import Control.Exception
-import Prelude hiding (catch)
-import System.Directory
-import System.Exit
-import System.FilePath
-import System.IO
-import System.IO.Error hiding (catch)
-import System.Process
-
+import Control.Monad.Trans
+import System.Console.Haskeline
+import qualified Evaluator as Eval
 import qualified Environment as Env
 
-main = loop Env.empty
+main = runInputT defaultSettings (loop Env.empty)
 
-loop :: Env.Repl -> IO ()
+loop :: Env.Repl -> InputT IO ()
 loop env = do
   str <- getInput
   case str of
     "" -> loop env
     _  -> do
       let env' = Env.insert str env
-      success <- runRepl env'
+      success <- liftIO $ Eval.runRepl env'
       loop (if success then env' else env)
 
-getInput :: IO String
 getInput = get "> " ""
     where
       get str old = do
-        putStr str
-        hFlush stdout
-        new <- getLine
-        continueWith (old ++ new)
+        input <- getInputLine str
+        case input of
+          Nothing  -> return old
+          Just new -> continueWith (old ++ new)
 
       continueWith str
         | null str || last str /= '\\' = return str
         | otherwise = get "| " (init str ++ "\n")
-
-runRepl :: Env.Repl -> IO Bool
-runRepl env =
-  do writeFile tempElm (Env.toElm env)
-     result <- onSuccess compile $ \types -> do
-       reformatJS tempJS
-       onSuccess run $ \value' ->
-           let value = BSC.init value'
-               tipe = scrapeOutputType types
-               isTooLong = or [ BSC.isInfixOf "\n" value
-                              , BSC.isInfixOf "\n" tipe
-                              , BSC.length value + BSC.length tipe > 80 ]    
-               message = BS.concat [ if isTooLong then value' else value, tipe ]
-           in  if BSC.null value' then return () else BSC.putStrLn message
-     removeIfExists tempElm
-     return result
-  where
-    tempElm = "repl-temp-000.elm"
-    tempJS  = "build" </> replaceExtension tempElm "js"
-    
-    run = (proc "node" [tempJS]) { std_out = CreatePipe }
-    compile = (proc "elm" args) { std_out = CreatePipe }
-        where args = [ "--make", "--only-js", "--print-types", tempElm ]
-
-    onSuccess action success =
-      let failure message = BSC.putStrLn message >> return False in
-      do (_, stdout, _, handle) <- createProcess action
-         exitCode <- waitForProcess handle
-         case (exitCode, stdout) of
-           (ExitFailure 127, _)      -> failure "Error: elm binary not found in your path."
-           (_, Nothing)              -> failure "Unknown error!"
-           (ExitFailure _, Just out) -> failure =<< BS.hGetContents out
-           (ExitSuccess  , Just out) ->
-               do success =<< BS.hGetContents out
-                  return True
-
-reformatJS :: String -> IO ()
-reformatJS tempJS =
-  do rts <- BS.readFile =<< Elm.runtime
-     src <- BS.readFile tempJS
-     BS.length src `seq` BS.writeFile tempJS (BS.concat [rts,src,out])
-  where
-    out = BS.concat
-          [ "var context = { inputs:[] };\n"
-          , "var repl = Elm.Repl.make(context);\n"
-          , "if (repl.", Env.output, ")\n"
-          , "  console.log(context.Native.Show.values.show(repl.", Env.output, "));" ]
-
-scrapeOutputType types
-    | name == Env.output = tipe
-    | BS.null rest       = ""
-    | otherwise          = scrapeOutputType rest
-    where
-      (next,rest) = freshLine types
-      (name,tipe) = BSC.splitAt (BSC.length Env.output) next
-
-      freshLine str
-          | BSC.take 2 rest == "\n " = (BS.append line line', rest')
-          | BS.null rest = (line,"")
-          | otherwise    = (line, BS.tail rest)
-          where
-            (line,rest) = BSC.break (=='\n') str
-            (line',rest') = freshLine rest
-
-removeIfExists :: FilePath -> IO ()
-removeIfExists fileName = removeFile fileName `catch` handleExists
-  where handleExists e
-          | isDoesNotExistError e = return ()
-          | otherwise = throwIO e
