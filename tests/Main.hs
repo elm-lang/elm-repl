ec1268b70ba93bf642701c211b1dfb8c10ef0945
Max New <maxsnew@gmail.com>
2014-01-15 01:55:57 -0600

    Prettify some tests

--- tests/Main.hs
+++ tests/Main.hs
@@ -19,38 +19,37 @@ main = defaultMain tests
 tests :: [Test]
 tests = [
   testGroup "Command parse tests" [
-     testCase ":help parses"  $ cmdParses Cmd.Help ":help"
+     testCase ":help parses"                      $ cmdParses Cmd.Help ":help"
      , testCase ":reset parses after whitespace"  $ cmdParses Cmd.Reset "  :reset"
      , testCase ":exit parses before whitespace"  $ cmdParses Cmd.Exit ":exit   "
      , testGroup ":flags parse tests" [
        testCase ":flags parses with Info" $ cmdParses Cmd.InfoFlags ":flags"
-       , testCase ":flags list parses" $ cmdParses Cmd.ListFlags ":flags list"
+       , testCase ":flags list parses"    $ cmdParses Cmd.ListFlags ":flags list"
        , testCase ":flags clear parses w/ whitespace between" $
-         cmdParses Cmd.ClearFlags ":flags     clear"
+           cmdParses Cmd.ClearFlags ":flags     clear"
        , testCase ":flags add source parses" $
-         cmdParses (Cmd.AddFlag "--src-dir=\"\"") ":flags add --src-dir=\"\""
+           cmdParses (Cmd.AddFlag "--src-dir=\"\"") ":flags add --src-dir=\"\""
        , testCase ":flags remove source parses" $
-         cmdParses (Cmd.RemoveFlag "--src-dir=bleh") ":flags remove --src-dir=bleh"
+           cmdParses (Cmd.RemoveFlag "--src-dir=bleh") ":flags remove --src-dir=bleh"
        ]
      , testProperty "bad :commands don't work" badCommandNoParse
      ]
   , testGroup "Code parse tests"  [
-     testCase "number parses"   $ codeParses "3"
-     , testCase "type def parses before whitespac"   $ codeParses "type Foo = Bar"
-     , testCase "data def parses after whitespace"   $ codeParses " data Baz = B { }"
-     , testCase "number parses after newlines" $ codeParses "\n\n3"
+     testCase "number parses"                       $ codeParses "3"
+     , testCase "type def parses before whitespace" $ codeParses "type Foo = Bar"
+     , testCase "data def parses after whitespace"  $ codeParses " data Baz = B { }"
+     , testCase "number parses after newlines"      $ codeParses "\n\n3"
      ]
   , testGroup "Whitespace parse tests" [
-     testCase "empty is skipped" $ skipped ""
+     testCase "empty is skipped"       $ skipped ""
      , testCase "newlines are skipped" $ skipped "\n\n\n"
-     , testProperty "skip all whitespace" $ skipAllSpace
+     , testProperty "skip all whitespace"       skipAllSpace
      , testProperty "never skip non-whitespace" dontSkipNonSpace
      ]
   ]
-  where codeParses src = actionParses (Action.Code . trimSpace $ src) src
+  where codeParses src    = actionParses (Action.Code . trimSpace $ src) src
         cmdParses cmd src = actionParses (Action.Command cmd) src
-        skipped src = actionParses Action.Skip src
-
+        skipped src       = actionParses Action.Skip src
         trimSpace = dropWhile Char.isSpace
 
 actionParses :: Action.Action -> String -> HUnit.Assertion
