75af975705922b29759830810140f732791c4465
Evan Czaplicki <info@elm-lang.org>
2014-04-02 14:12:19 -0700

    Cosmetic: more indentation and trading vertical space for regularity

--- src/Command.hs
+++ src/Command.hs
@@ -14,35 +14,56 @@ import Monad (ReplM)
 run :: Command -> ReplM (Maybe ExitCode)
 run cmd =
   case cmd of
-    Exit         -> return (Just ExitSuccess)
-    Help m       -> displayErr "Bad command\n" m >> display helpInfo
-    InfoFlags m  -> displayErr "Bad flag\n" m    >> display flagsInfo
-    ListFlags    -> display . unlines . Env.flags =<< get
+    Exit -> return (Just ExitSuccess)
 
-    AddFlag flag -> modifyIfPresent True flag "Added " "Flag already added!" $ \env ->
-        env { Env.flags = Env.flags env ++ [flag] }
+    Help m ->
+        do displayErr "Bad command\n" m
+           display helpInfo
 
-    RemoveFlag flag -> modifyIfPresent False flag "Removed flag " "No such flag." $ \env ->
-      env {Env.flags = List.delete flag $ Env.flags env}
+    InfoFlags m ->
+        do displayErr "Bad flag\n" m
+           display flagsInfo
 
-    Reset -> modifyAlways "Environment Reset" (Env.empty . Env.compilerPath)
-    ClearFlags -> modifyAlways "All flags cleared" $ \env ->
-      env {Env.flags = []}
+    ListFlags ->
+        display . unlines . Env.flags =<< get
 
-  where display msg = Nothing <$ (liftIO . putStrLn $ msg)
-        displayErr msg m = case m of
+    AddFlag flag ->
+        modifyIfPresent True flag "Added " "Flag already added!" $ \env ->
+            env { Env.flags = Env.flags env ++ [flag] }
+
+    RemoveFlag flag ->
+        modifyIfPresent False flag "Removed flag " "No such flag." $ \env ->
+            env {Env.flags = List.delete flag $ Env.flags env}
+
+    Reset ->
+        modifyAlways "Environment Reset" (Env.empty . Env.compilerPath)
+
+    ClearFlags ->
+        modifyAlways "All flags cleared" $ \env ->
+            env {Env.flags = []}
+
+  where
+    display msg =
+        do liftIO . putStrLn $ msg
+           return Nothing
+
+    displayErr msg m =
+        case m of
           Nothing -> return ()
-          Just err -> liftIO . putStrLn $ (msg ++ err)
-        modifyIfPresent b flag msgSuc msgFail mod = do
-          env <- get
-          if not b `xor` (flag `elem` Env.flags env)
-            then display msgFail
-            else Nothing <$ do
-          liftIO . putStrLn $ msgSuc ++ flag
-          modify mod
-        modifyAlways msg mod = Nothing <$ do
-          liftIO . putStrLn $ msg
-          modify mod
+          Just err -> liftIO . putStrLn $ msg ++ err
+
+    modifyIfPresent b flag msgSuc msgFail mod =
+        do env <- get
+           if not b `xor` (flag `elem` Env.flags env)
+             then display msgFail
+             else do liftIO . putStrLn $ msgSuc ++ flag
+                     modify mod
+                     return Nothing
+
+    modifyAlways msg mod =
+        do liftIO . putStrLn $ msg
+           modify mod
+           return Nothing
 
 xor :: Bool -> Bool -> Bool
 xor True  = not
