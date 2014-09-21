5db8289ce00a34edc640e1559fbda1efafac10bd
Evan Czaplicki <info@elm-lang.org>
2014-09-21 15:08:17 -0700

    style: conform to new style guide

--- src/Evaluator.hs
+++ src/Evaluator.hs
@@ -1,8 +1,6 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Evaluator where
 
-import Control.Applicative ((<$>), (<*>))
-import Control.Monad (unless)
 import Control.Monad.Cont (ContT(ContT, runContT))
 import Control.Monad.RWS (get, modify)
 import Control.Monad.Trans (liftIO)
@@ -22,41 +20,62 @@ import Monad (ReplM)
 
 evalPrint :: Term -> ReplM ()
 evalPrint term =
-  do modify $ Env.insert term 
-     env <- get
-     liftIO $ writeFile tempElm $ Env.toElm env
-     let elmArgs = Env.flags env ++ ["--make", "--only-js", "--print-types", tempElm]
-     liftIO . runConts $
-       do types  <- runCmd (Env.compilerPath env) elmArgs
-          liftIO $ reformatJS tempJS
-          value' <- runCmd (Env.interpreterPath env) nodeArgs
-          let value = BSC.init value'
-              tipe = scrapeOutputType types
-              isTooLong = BSC.isInfixOf "\n" value ||
-                          BSC.isInfixOf "\n" tipe ||
-                          BSC.length value + BSC.length tipe > 80   
-              message = BS.concat [ if isTooLong then value' else value, tipe ]
-            in liftIO $ unless (BSC.null value') $ BSC.hPutStrLn stdout message
-     liftIO $ removeIfExists tempElm
-     return ()
+ do modify $ Env.insert term 
+    env <- get
+    liftIO $ writeFile tempElmPath (Env.toElm env)
+    liftIO . runConts $ do
+        types <- runCmd (Env.compilerPath env) (Env.flags env ++ elmArgs)
+        liftIO $ reformatJS tempJsPath
+        value <- runCmd (Env.interpreterPath env) [tempJsPath]
+        liftIO $ printIfNeeded value (scrapeOutputType types)
+    liftIO $ removeIfExists tempElmPath
+    return ()
   where
-
     runConts m = runContT m (\_ -> return ())
     
-    tempElm = "repl-temp-000" <.> "elm"
-    tempJS  = "build" </> replaceExtension tempElm "js"
+    tempElmPath =
+        "repl-temp-000" <.> "elm"
+
+    tempJsPath =
+        "build" </> replaceExtension tempElmPath "js"
     
-    nodeArgs = [tempJS]
+    elmArgs =
+        [ "--make"
+        , "--only-js"
+        , "--print-types"
+        , tempElmPath
+        ]
+
+printIfNeeded :: BS.ByteString -> BS.ByteString -> IO ()
+printIfNeeded rawValue tipe =
+    if BSC.null rawValue
+      then return ()
+      else BSC.hPutStrLn stdout message
+  where
+    value = BSC.init rawValue
+
+    isTooLong =
+        BSC.isInfixOf "\n" value
+        || BSC.isInfixOf "\n" tipe
+        || BSC.length value + BSC.length tipe > 80
+
+    message =
+        BS.concat
+            [ if isTooLong then rawValue else value
+            , tipe
+            ]
+
 
 runCmd :: FilePath -> [String] -> ContT () IO BS.ByteString
 runCmd name args = ContT $ \ret ->
-  do (exitCode, stdout, stderr) <- liftIO $ readProcessWithExitCode name args ""
-     case exitCode of
-       ExitSuccess -> ret (BSC.pack stdout)
-       ExitFailure code
-           | code == 127  -> failure missingExe  -- UNIX
-           | code == 9009 -> failure missingExe  -- Windows
-           | otherwise    -> failure (stdout ++ stderr)
+  do  (exitCode, stdout, stderr) <-
+          liftIO $ readProcessWithExitCode name args ""
+      case exitCode of
+        ExitSuccess -> ret (BSC.pack stdout)
+        ExitFailure code
+            | code == 127  -> failure missingExe  -- UNIX
+            | code == 9009 -> failure missingExe  -- Windows
+            | otherwise    -> failure (stdout ++ stderr)
   where
     failure message = liftIO $ hPutStrLn stderr message
 
@@ -66,43 +85,77 @@ runCmd name args = ContT $ \ret ->
         , "  Do you have it installed?"
         , "  Can it be run from anywhere? I.e. is it on your PATH?" ]
 
+
 reformatJS :: String -> IO ()
-reformatJS tempJS =
-  do rts <- BS.readFile Elm.runtime
-     src <- BS.readFile tempJS
-     BS.length src `seq` BS.writeFile tempJS (BS.concat [rts,src,out])
+reformatJS tempJsPath =
+  do  rts <- BS.readFile Elm.runtime
+      src <- BS.readFile tempJsPath
+      BS.length src `seq` BS.writeFile tempJsPath (BS.concat [rts,src,out])
   where
-    out = BS.concat
-          [ "process.on('uncaughtException', function(err) {\n"
-          , "  process.stderr.write(err.toString());\n"
-          , "  process.exit(1);\n"
-          , "});\n"
-          , "var document = document || {};"
-          , "var window = window || {};"
-          , "var context = { inputs:[], addListener:function(){}, node:{} };\n"
-          , "var repl = Elm.Repl.make(context);\n"
-          , "var show = Elm.Native.Show.make(context).show;"
-          , "if ('", Env.lastVar, "' in repl)\n"
-          , "  console.log(show(repl.", Env.lastVar, "));" ]
+    out =
+        BS.concat
+            [ "process.on('uncaughtException', function(err) {\n"
+            , "  process.stderr.write(err.toString());\n"
+            , "  process.exit(1);\n"
+            , "});\n"
+            , "var document = document || {};"
+            , "var window = window || {};"
+            , "var context = { inputs:[], addListener:function(){}, node:{} };\n"
+            , "var repl = Elm.Repl.make(context);\n"
+            , "var show = Elm.Native.Show.make(context).show;"
+            , "if ('", Env.lastVar, "' in repl)\n"
+            , "  console.log(show(repl.", Env.lastVar, "));"
+            ]
+
 
 scrapeOutputType :: BS.ByteString -> BS.ByteString
-scrapeOutputType = dropName . squashSpace . takeType . dropWhile (not . isOut) . BSC.lines
-  where isOut    = (||) <$> (BS.isPrefixOf Env.lastVar) <*> (BS.isPrefixOf (BS.append "Repl." Env.lastVar))
-        dropName = BSC.cons ' ' . BSC.dropWhile (/= ':')
-        takeType (n:rest) = n : takeWhile isMoreType rest
-        takeType []       = error "Internal error in elm-repl (takeType): Please report this bug to https://github.com/elm-lang/elm-repl/issues/"
-        isMoreType = (&&) <$> not . BS.null <*> (Char.isSpace . BSC.head)
-        squashSpace = BSC.unwords . BSC.words . BSC.unwords
+scrapeOutputType rawTypeDump =
+    dropName (squashSpace relevantLines)
+  where
+    squashSpace :: [BS.ByteString] -> BS.ByteString
+    squashSpace multiLineTypeDecl =
+        BSC.unwords (BSC.words (BSC.unwords multiLineTypeDecl))
+
+    dropName :: BS.ByteString -> BS.ByteString
+    dropName typeDecl =
+        BSC.cons ' ' (BSC.dropWhile (/= ':') typeDecl)
+
+    relevantLines :: [BS.ByteString]
+    relevantLines =
+        takeType . dropWhile (not . isLastVar) $ BSC.lines rawTypeDump
+
+    isLastVar :: BS.ByteString -> Bool
+    isLastVar line =
+        BS.isPrefixOf Env.lastVar line
+        || BS.isPrefixOf (BS.append "Repl." Env.lastVar) line
+
+    takeType :: [BS.ByteString] -> [BS.ByteString]
+    takeType lines =
+        case lines of
+          [] -> error errorMessage
+          line : rest ->
+              line : takeWhile isMoreType rest
+
+    isMoreType :: BS.ByteString -> Bool
+    isMoreType line =
+        not (BS.null line)
+        && Char.isSpace (BSC.head line)
+
+    errorMessage =
+        "Internal error in elm-repl function scrapeOutputType\n\
+        \Please report this bug to <https://github.com/elm-lang/elm-repl/issues>"
+
 
 freshLine :: BS.ByteString -> (BS.ByteString, BS.ByteString)
-freshLine str | BS.null rest' = (line,"")
-              | otherwise     = (line, BS.tail rest')
+freshLine str
+    | BS.null rest' = (line, "")
+    | otherwise     = (line, BS.tail rest')
   where
     (line,rest') = BSC.break (=='\n') str
 
 removeIfExists :: FilePath -> IO ()
-removeIfExists fileName = do
-  exists <- doesFileExist fileName
-  if exists
-    then removeFile fileName
-    else return ()
+removeIfExists fileName =
+    do  exists <- doesFileExist fileName
+        if exists
+          then removeFile fileName
+          else return ()
