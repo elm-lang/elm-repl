0901b4f810359c6d5634d455e299e4326685f805
Evan Czaplicki <info@elm-lang.org>
2014-09-21 13:53:58 -0700

    style: use 4 space indent and make things a bit easier to read

--- src/Flags.hs
+++ src/Flags.hs
@@ -13,14 +13,25 @@ data Flags = Flags
     , interpreter :: FilePath
     }
     deriving (Data,Typeable,Show,Eq)
-             
+
+flags :: Flags             
 flags = Flags
-  { compiler = "elm"  &= typFile
-                      &= help "Provide a path to a specific Elm compiler."
-  , interpreter = "node"  &= typFile
-                          &= help "Provide a path to a specific JavaScript interpreter (e.g. node, nodejs, ...)."
-  } &= help "Read-eval-print-loop (REPL) for digging deep into Elm projects.\n\
-            \More info at <https://github.com/elm-lang/elm-repl#elm-repl>"
-    &= helpArg [explicit, name "help", name "h"]
-    &= versionArg [explicit, name "version", name "v", summary version]
-    &= summary ("Elm REPL " ++ version ++ ", (c) Evan Czaplicki 2011-2013")
+    { compiler = "elm"
+        &= typFile
+        &= help "Provide a path to a specific Elm compiler."
+    , interpreter = "node" 
+        &= typFile
+        &= help "Provide a path to a specific JavaScript interpreter (e.g. node, nodejs, ...)."
+    }
+        &= help helpMessage
+
+        &= helpArg [explicit, name "help", name "h"]
+
+        &= versionArg [explicit, name "version", name "v", summary version]
+
+        &= summary ("Elm REPL " ++ version ++ ", (c) Evan Czaplicki 2011-2013")
+
+helpMessage :: String
+helpMessage =
+    "Read-eval-print-loop (REPL) for digging deep into Elm projects.\n\
+    \More info at <https://github.com/elm-lang/elm-repl#elm-repl>"
