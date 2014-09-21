0cf71ac2f60adf9ce78245c9f0a98f242c48c097
Evan Czaplicki <info@elm-lang.org>
2014-09-21 15:22:34 -0700

    style: conform to new style guide

--- src/Monad.hs
+++ src/Monad.hs
@@ -1,16 +1,17 @@
-module Monad (ReplM, runReplM)
-       where
+module Monad (ReplM, runReplM) where
 
 import Control.Monad.RWS (RWST, runRWST)
-import Data.Functor ((<$>))
-
 import qualified Environment as Env
-import Flags (Flags)
+import qualified Flags
+
 
 -- Reader: Build Flags
 -- State:  Current Environment
-type ReplT = RWST Flags () Env.Repl
+type ReplT = RWST Flags.Flags () Env.Repl
 type ReplM = ReplT IO
 
-runReplM :: Flags -> Env.Repl -> ReplM a -> IO a
-runReplM fs repl m = (\(x,_,_) -> x) <$> runRWST m fs repl
+
+runReplM :: Flags.Flags -> Env.Repl -> ReplM a -> IO a
+runReplM flags env command =
+    do  (x,_,_) <- runRWST command flags env
+    	return x
