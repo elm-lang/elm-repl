9ef9b17e2014adab2cf5083b02a31dcbf4ab6fec
Evan Czaplicki <info@elm-lang.org>
2014-09-21 13:56:33 -0700

    style: follow new style guide for indentation
    https://gist.github.com/evancz/0a1f3717c92fe71702be

--- src/Action.hs
+++ src/Action.hs
@@ -1,24 +1,27 @@
 module Action where
 
-data Action = Command Command
-            | Code Term
-            | Skip
-            deriving (Show, Eq)
+data Action
+    = Command Command
+    | Code Term
+    | Skip
+    deriving (Show, Eq)
 
-data Command = AddFlag String
-             | RemoveFlag String
-             | ListFlags
-             | ClearFlags
-               -- Just if this was triggered by an error
-             | InfoFlags (Maybe String)
-             | Help (Maybe String)
-             | Exit
-             | Reset
-             deriving (Show, Eq)
+data Command
+    = AddFlag String
+    | RemoveFlag String
+    | ListFlags
+    | ClearFlags
+      -- Just if this was triggered by an error
+    | InfoFlags (Maybe String)
+    | Help (Maybe String)
+    | Exit
+    | Reset
+    deriving (Show, Eq)
 
 type Term = (String, Maybe Def)
 
-data Def = VarDef  String
-         | DataDef String
-         | Import  String
-         deriving (Show, Eq)
+data Def
+    = VarDef  String
+    | DataDef String
+    | Import  String
+    deriving (Show, Eq)
